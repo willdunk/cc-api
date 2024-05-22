34b774030a9bc4921de9cec88a425a36b6b6626e
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-21 22:26:55 -0400

    feat(*): Add an Authenticated GET /users endpoint

--- src/routers/login.ts
+++ src/routers/login.ts
@@ -1,31 +1,16 @@
 import express, { Request, Response } from 'express';
-import { RefreshToken, User } from '../models/user';
-import bcrypt from 'bcryptjs';
-import { encodeAndSaveToken } from '../modules/jwt/encodeAndSaveToken';
 import { StatusCodes } from 'http-status-codes'
 import { PostLoginInputSchema } from '../schemas/login/PostLoginInputSchema';
 import { PostRefreshInputSchema } from '../schemas/login/PostRefreshInputSchema';
-import { decode } from '../modules/jwt/decode';
-import { isDefined } from '../utils/ts/isDefined';
+import { login } from '../modules/auth/login';
+import { refresh } from '../modules/auth/refresh';
 
 const router = express.Router();
 
 router.post('/', async (req: Request, res: Response) => {
     try {
         const { email, password } = PostLoginInputSchema.validateSync(req.body);
-        const user = await User.findOne({ email });
-        if (!user) {
-            res.status(StatusCodes.NOT_FOUND).json({ message: "User Not Found" });
-            return;
-        }
-
-        const isMatch = await bcrypt.compare(password, user.passwordHash);
-        if (!isMatch) {
-            res.status(StatusCodes.UNAUTHORIZED).json({ message: "Invalid credentials" });
-            return;
-        }
-
-        const newTokens = await encodeAndSaveToken(user._id);
+        const newTokens = await login(email, password);
         res.status(StatusCodes.OK).json(newTokens);
     } catch (error) {
         console.error(error);
@@ -36,23 +21,7 @@ router.post('/', async (req: Request, res: Response) => {
 router.post('/refresh', async (req: Request, res: Response) => {
     try {
         const { refreshToken } = PostRefreshInputSchema.validateSync(req.body);
-        const decodedUser = decode(refreshToken, 'refresh');
-        const user = await User.findById(decodedUser.userId);
-        if (!user) {
-            res.status(StatusCodes.NOT_FOUND).json({ message: "User Not Found" });
-            return;
-        }
-
-        const matchingToken: RefreshToken | undefined = user.refreshTokenHashes?.find(token => {
-            bcrypt.compareSync(refreshToken, token.refreshTokenHash);
-        });
-
-        if (isDefined(matchingToken) && matchingToken.expiresOn < new Date()) {
-            res.status(StatusCodes.UNAUTHORIZED).json({ message: "Refresh Token Not Valid" });
-            return;
-        }
-
-        const newTokens = await encodeAndSaveToken(user._id);
+        const newTokens = await refresh(refreshToken);
         res.status(StatusCodes.OK).json(newTokens);
     } catch (error) {
         console.error(error);
--- src/routers/user.ts
+++ src/routers/user.ts
@@ -2,6 +2,11 @@ import express, { Request, Response } from 'express';
 import { User } from '../models/user';
 import { createNewUser } from '../modules/user/createNewUser';
 import { PostUserInputSchema } from '../schemas/user/PostUserInputSchema';
+import { authenticateUser } from '../middlewares/auth';
+import { StatusCodes } from 'http-status-codes';
+import { isDefined } from '../utils/ts/isDefined';
+import { GetUserOutputSchema } from '../schemas/user/GetUserOutputSchema';
+import { filterObject } from '../utils/filterObject';
 
 const router = express.Router();
 
@@ -16,9 +21,23 @@ router.post('/', async (req: Request, res: Response) => {
     }
 });
 
-router.get('/', async (req: Request, res: Response) => {
-    const users = await User.find().lean().exec();
-    res.json(users);
+router.get('/', authenticateUser, async (req: Request, res: Response) => {
+    try {
+        const userId = req.userId;
+        if (!isDefined(userId)) {
+            return res.status(StatusCodes.UNAUTHORIZED).json({ message: 'Unauthorized to view this user' });
+        }
+        const user = await User.findById(userId).lean().exec();
+        if (!isDefined(user)) {
+            return res.status(StatusCodes.NOT_FOUND).json({ message: 'User not found' });
+        }
+
+        res.status(StatusCodes.OK).json(filterObject(user, GetUserOutputSchema));
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
 });
 
 export default router;
