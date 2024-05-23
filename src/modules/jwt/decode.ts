20278d34934d4255db9bc3e3f3c602cb96ab5ca3
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-22 22:07:25 -0400

    test

--- src/modules/jwt/decode.ts
+++ src/modules/jwt/decode.ts
@@ -1,9 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { isJWTBody, type JWTBody, type TokenType } from './types';
 import { secrets, secretsMap } from './secrets';
+import { isDefined } from '../../utils/ts/isDefined';
 
 export const decode = (token: string, tokenType: TokenType): JWTBody => {
-    const decodedBody = jwt.verify(token, secrets[secretsMap[tokenType]]);
+    const secret = secrets[secretsMap[tokenType]];
+    if (!isDefined(secret)) {
+        throw new Error("Cannot find jwt secret key");
+    }
+    const decodedBody = jwt.verify(token, secret);
     if (isJWTBody(decodedBody)) return decodedBody
     throw new Error("Cannot decode jwt token");
 }
