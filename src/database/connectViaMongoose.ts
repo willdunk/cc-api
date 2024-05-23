de7d5c0460d2f1ff9bd03a7dc8d54c99be30eaa4
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-22 23:06:36 -0400

    Remove console.log

--- src/database/connectViaMongoose.ts
+++ src/database/connectViaMongoose.ts
@@ -3,7 +3,6 @@ import { isDefined } from '../utils/ts/isDefined';
 
 export const connectViaMongoose = () => {
     if (isDefined(process.env.DB_CONNECTION_STRING)) {
-        console.log(process.env.DB_CONNECTION_STRING);
         return mongoose.connect(process.env.DB_CONNECTION_STRING);
     }
     return Promise.reject(new Error("Cannot connect to mongo"));
