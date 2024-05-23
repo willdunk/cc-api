0b01bd81b53dc28aa6e8cfb64c5058aaaa0af3ba
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-22 22:17:02 -0400

    test

--- src/database/connectViaMongoose.ts
+++ src/database/connectViaMongoose.ts
@@ -3,7 +3,7 @@ import { isDefined } from '../utils/ts/isDefined';
 
 export const connectViaMongoose = () => {
     if (isDefined(process.env.DB_HOST)) {
-        return mongoose.connect(process.env.DB_HOST, { dbName: process.env.DB_NAME, pass: process.env.DB_PASS, user: process.env.DB_USER });
+        return mongoose.connect(process.env.DB_CONNECTION_STRING);
     }
     return Promise.reject(new Error("Cannot connect to mongo"));
 }
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,6 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        DB_USERNAME: string;
-        DB_PASSWORD: string;
-        DB_HOST: string;
-        DB_DATABASE: string;
+        DB_CONNECTION_STRING: string;
         ENV: string;
         JWT_ACCESS_TOKEN_SECRET: string;
         JWT_REFRESH_TOKEN_SECRET: string;
