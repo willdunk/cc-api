1a703087178640c571bb7be38bb25445464f6827
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-22 22:18:38 -0400

    fix

--- src/database/connectViaMongoose.ts
+++ src/database/connectViaMongoose.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { isDefined } from '../utils/ts/isDefined';
 
 export const connectViaMongoose = () => {
-    if (isDefined(process.env.DB_HOST)) {
+    if (isDefined(process.env.DB_CONNECTION_STRING)) {
         return mongoose.connect(process.env.DB_CONNECTION_STRING);
     }
     return Promise.reject(new Error("Cannot connect to mongo"));
