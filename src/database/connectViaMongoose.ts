46a1ec8fa3d9487e3a1ac84890f61eab1cabb9ac
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-28 22:04:47 -0400

    test

--- src/database/connectViaMongoose.ts
+++ src/database/connectViaMongoose.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { isDefined } from '../utils/ts/isDefined';
 
 export const connectViaMongoose = () => {
+    console.log(process.env.DATABASE_URL);
     if (isDefined(process.env.DATABASE_URL)) {
         return mongoose.connect(process.env.DATABASE_URL);
     }
