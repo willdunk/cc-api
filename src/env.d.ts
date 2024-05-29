a8c328d3bc23e621fafba2e9e4b99d1eb1c58d4c
William Dunkerley <will.eric.dunkerley@gmail.com>
2024-05-28 22:01:39 -0400

    fix(*): Change to DATABASE_URL

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 declare namespace NodeJS {
     export interface ProcessEnv {
-        DB_CONNECTION_STRING: string;
-        ENV: string;
+        DATABASE_URL: string;
         JWT_ACCESS_TOKEN_SECRET: string;
         JWT_REFRESH_TOKEN_SECRET: string;
         PORT: string;
